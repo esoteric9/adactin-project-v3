a2baa865c08d6e9dc9f95ec42b4b4836a5c50d48
amunteanu9 <maxcool.alex@gmail.com>
2017-06-28 14:27:46 -0700

    Update Core

--- src/main/java/com/amunteanu/helpers/Core.java
+++ src/main/java/com/amunteanu/helpers/Core.java
@@ -7,10 +7,11 @@
  */
 package com.amunteanu.helpers;
 
-import java.io.*;
+import java.io.IOException;
 
-import org.apache.log4j.*;
-import org.openqa.selenium.*;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 /**
  * Core //ADDD (description of class)
@@ -23,50 +24,61 @@ import org.openqa.selenium.*;
  * @version 1.0.0
  * @since 1.0
  */
-public class Core {
+public class Core
+{
 
 	private WebDriver driver;
 
 	private Logger logger;
 
-	public Core() {
+	public Core()
+	{
 		this.logger = Logger.getLogger(Core.class);
 	}
 
-	public Core(WebDriver driver) {
+	public Core(WebDriver driver)
+	{
 		this.driver = driver;
 		this.logger = Logger.getLogger(Core.class);
 	}
 
-	public void addProp(String key, String value) {
+	public void addProp(String key, String value)
+	{
 		AutoBasics.addProp(key, value);
 	}
 
-	public WebDriver getDriver() {
+	public WebDriver getDriver()
+	{
 		return this.driver;
 	}
 
-	protected void setDriver(WebDriver driver) {
+	protected void setDriver(WebDriver driver)
+	{
 		this.driver = driver;
 	}
 
-	public int getInt(String name) throws IOException {
+	public int getInt(String name) throws IOException
+	{
 		return AutoBasics.getInt(name);
 	}
 
-	public Logger getLog() {
+	public Logger getLog()
+	{
 		return this.logger;
 	}
 
-	public String getProp(String name) throws IOException {
+	public String getProp(String name) throws IOException
+	{
 		return AutoBasics.getProp(name);
 	}
 
-	public boolean isElementPresent(By by) {
+	public boolean isElementPresent(By by)
+	{
 		return AutoBasics.isElementPresent(getDriver(), by);
 	}
 
-	public boolean takeScreeshot(String name) {
+	public boolean takeScreenshot(String name)
+	{
 		return AutoBasics.takeScreenshot(getDriver(), name);
 	}
 }
